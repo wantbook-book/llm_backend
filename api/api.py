9841d10b8bcaf19c8715e00634be02fe7055ffcf
akai <2780896955@qq.com>
2024-05-13 22:12:02 +0800

    增加config文件，方便配置

--- api/api.py
+++ api/api.py
@@ -10,40 +10,47 @@ def create_app(models)->FastAPI:
     @app.get("/ping")
     def ping():
         return {"data": "pong!"}
-
-
+    def create_route(model_name):
+        @app.post(f"/{model_name}", response_model=LlamaResponse)
+        def llama(llama_request: LlamaRequest):
+            response = models[model_name](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+            return {"status": 0, "data": response}
+        
+    for name in models.keys():
+        create_route(name)
+        
     # @app.post("/llama", response_model=LlamaResponse)
     # def llama(llama_request: LlamaRequest):
     #     response = models['v1'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
     #     return {"status": 0, "data": response}
     
-    @app.post("/llama2_70b", response_model=LlamaResponse)
-    def llama(llama_request: LlamaRequest):
-        response = models['llama2_70b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
-        return {"status": 0, "data": response}
+    # @app.post("/llama2_70b", response_model=LlamaResponse)
+    # def llama(llama_request: LlamaRequest):
+    #     response = models['llama2_70b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+    #     return {"status": 0, "data": response}
     
-    @app.post("/llama3_70b", response_model=LlamaResponse)
-    def llama(llama_request: LlamaRequest):
-        response = models['llama3_70b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
-        return {"status": 0, "data": response}
+    # @app.post("/llama3_70b", response_model=LlamaResponse)
+    # def llama(llama_request: LlamaRequest):
+    #     response = models['llama3_70b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+    #     return {"status": 0, "data": response}
     
-    @app.post("/llama3_8b", response_model=LlamaResponse)
-    def llama(llama_request: LlamaRequest):
-        response = models['llama3_8b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
-        return {"status": 0, "data": response}
+    # @app.post("/llama3_8b", response_model=LlamaResponse)
+    # def llama(llama_request: LlamaRequest):
+    #     response = models['llama3_8b'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+    #     return {"status": 0, "data": response}
     
     # @app.post("/llama2", response_model=LlamaResponse)
     # def llama(llama_request: LlamaRequest):
     #     response = models['llama2'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
     #     return {"status": 0, "data": response}
     
-    @app.post("/llama2_13b_v3", response_model=LlamaResponse)
-    def llama(llama_request: LlamaRequest):
-        response = models['llama2_13b_v3'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
-        return {"status": 0, "data": response}
+    # @app.post("/llama2_13b_v3", response_model=LlamaResponse)
+    # def llama(llama_request: LlamaRequest):
+    #     response = models['llama2_13b_v3'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+    #     return {"status": 0, "data": response}
 
-    @app.post("/llama2_13b_v4", response_model=LlamaResponse)
-    def llama(llama_request: LlamaRequest):
-        response = models['llama2_13b_v4'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
-        return {"status": 0, "data": response}
+    # @app.post("/llama2_13b_v4", response_model=LlamaResponse)
+    # def llama(llama_request: LlamaRequest):
+    #     response = models['llama2_13b_v4'](user_prompt=llama_request.user_prompt, system_prompt=llama_request.system_prompt)
+    #     return {"status": 0, "data": response}
     return app
--- main.py
+++ main.py
@@ -5,20 +5,23 @@ from api.api import create_app
 from model.llama import Llama
 import uvicorn
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7" #指定cuda可见显卡编号
-
+from conf import config_manager
+os.environ["CUDA_VISIBLE_DEVICES"] = config_manager.get('CUDA_VISIBLE_DEVICES') #指定cuda可见显卡编号
 if __name__ == "__main__":
+    models_path = config_manager.get('models')
 
     models = {
+        name: Llama(model_path=models_path[name], device_map="auto")
+        for name in models_path
     }
     # os.environ["CUDA_VISIBLE_DEVICES"] = "0" #指定cuda可见显卡编号
     # models['v1'] = Llama(model_path="/home/jovyan/notebook/MineLLaMa", device_map={"":5})
-    models['llama3_8b'] = Llama(model_path="/home/jovyan/notebook/LLAMA-3-8B", device_map="auto")
+    # models['llama3_8b'] = Llama(model_path="/home/jovyan/notebook/LLAMA-3-8B", device_map="auto")
     # models['llama2'] = Llama(model_path="/home/jovyan/notebook/LLAMA_13B", device_map="auto")
-    models['llama3_70b'] = Llama(model_path="/pubshare/LLM/Meta-Llama-3-70B-Instruct", device_map="auto")
-    models['llama2_13b_v3'] = Llama(model_path="/home/jovyan/notebook/MineLLaMa-v3", device_map="auto")
-    models['llama2_13b_v4'] = Llama(model_path="/home/jovyan/notebook/MineLLaMa-v4", device_map="auto")
-    models['llama2_70b'] = Llama(model_path="/pubshare/LLM/Llama-2-70b-chat-hf", device_map="auto")
+    # models['llama3_70b'] = Llama(model_path="/pubshare/LLM/Meta-Llama-3-70B-Instruct", device_map="auto")
+    # models['llama2_13b_v3'] = Llama(model_path="/home/jovyan/notebook/MineLLaMa-v3", device_map="auto")
+    # models['llama2_13b_v4'] = Llama(model_path="/home/jovyan/notebook/MineLLaMa-v4", device_map="auto")
+    # models['llama2_70b'] = Llama(model_path="/pubshare/LLM/Llama-2-70b-chat-hf", device_map="auto")
     
     app = create_app(models)
-    uvicorn.run(app, host="0.0.0.0", port=9999)
+    uvicorn.run(app, host="0.0.0.0", port=config_manager.get('port'))
